51a67adec81399bc40a3e9903826a6492489aa83
msrana25 <mnprtrana61@gmail.com>
2023-05-08 16:41:38 -0400

    fixed issues while deploying to Vercel

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import ClientOnly from "./components/ClientOnly";
 import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
 import ListingCard from "./components/listings/ListingCard";
+export const dynamic = 'force-dynamic';
+
 
 interface HomeProps {
   searchParams: IListingsParams
